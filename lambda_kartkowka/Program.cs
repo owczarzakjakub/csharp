d26bb0d5ec3368701e73eb59314fda7e7c4891f4
owczarzakjakub <144985790+owczarzakjakub@users.noreply.github.com>
2024-01-31 21:09:16 +0100

    Add files via upload

---
+++ lambda_kartkowka/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Xml.Serialization;
+
+namespace lambda_kartkowka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int n = ReadInt("Podaj rozmiar tablicy: ", 0, int.MaxValue);
+            int[] tab = new int[n];
+            tab[0] = 1;
+            int it = 0;
+            foreach (int i in tab)
+            {
+                it = it + 1;
+                Console.Write(i + ", ");
+                if (it == n)
+                {
+                    Console.WriteLine();
+                    it = 0;
+                }
+            }
+            */
+
+            int n = ReadInt("Podaj liczbę osób:", 0, int.MaxValue);
+            Console.WriteLine();
+
+            string[] names = new string[n];
+            int[] ages = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                names[i] = ReadString("podaj imie: ");
+                ages[i] = ReadInt("podaj wiek: ", 0, 112);
+            }
+            Console.WriteLine("Persons: ");
+            Printarray(names, ages);
+
+            List<string> NamesStartsWitha = names.Where(e => e.StartsWith("A", StringComparison.OrdinalIgnoreCase)).ToList();
+
+            Console.WriteLine("Persons which names starts with letter A: ");
+            PrintList(NamesStartsWitha);
+            Console.WriteLine();
+
+            Dictionary<string, int> adults = new Dictionary<string, int>();
+            for (int i = 0;i < n;i++)
+            {
+                if (ages[i] >= 18)
+                {
+                    adults.Add(names[i], ages[i]);
+                }
+            }
+            Console.WriteLine("Adults: ");
+            PrintDictionary(adults);
+            
+
+        }
+        static int ReadInt(string prompt, int min, int max)
+        {
+
+            bool vaild;
+            int result;
+            Console.Write(prompt);
+            do
+            {
+                vaild = int.TryParse(Console.ReadLine(), out result) && result >= min && result <= max;
+                if (vaild == false || result <= min || result > max)
+                {
+                    Console.WriteLine($"Podaj liczbe od {min} do {max}");
+                }
+
+            } while (vaild == false);
+            return result;
+        }
+        static string ReadString(string prompt)
+        {
+            string result;
+            do
+            {
+                Console.Write(prompt);
+                result = Console.ReadLine();
+                if (string.IsNullOrEmpty(result))
+                {
+                    Console.WriteLine("Podaj nie pusty ciag znaków: ");
+                }
+            } while (string.IsNullOrEmpty(result));
+            return result;
+        }
+        static void Printarray(string[] names, int[] ages)
+        {
+            for (int i = 0;i < names.Length;i++)
+            {
+                Console.WriteLine($"{names[i]} - {ages[i]}");
+            }
+        }
+        static void PrintList(List<string> names)
+        {
+            foreach (string name in names)
+            {
+                Console.Write(name + ", ");
+            }
+        }
+        static void PrintDictionary(Dictionary<string, int> adults) {
+            
+            foreach(var adult in adults)
+            {
+                Console.WriteLine($"{adult.Key} - {adult.Value}");
+            }
+            
+        }
+    }
+}
---
+++ project_powtorka/Program.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Specialized;
+using System.Diagnostics.Metrics;
+
+namespace project_powtorka_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> kraje = new List<string>() { "Polska", "Rosja", "Niemcy" };
+            List<string> stolice = new List<string>() { "Warszawa", "Moskwa", "Berlin" };
+            List<int> list = CreateList(4);
+            FillList(list);
+            PrintList(list);
+            int[,] multiarray = CreateMultiDimensionalarray(2);
+            fillarray(multiarray);
+            int it;
+            Console.WriteLine("Tablica: ");
+            for (int i = 0; i < multiarray.GetLength(0); i++)
+            {
+                it = 0;
+                Console.WriteLine();
+                for (int j = 0; j < multiarray.GetLength(1); j++)
+                {
+                    it++;
+                    if(it < 2)
+                    {
+                        Console.Write(multiarray[i, j] + ", ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(multiarray[i, j]);
+                    }
+                    
+                }
+            }
+            SumaPrzekatnej(multiarray);
+            Dictionary<string, string> krajestolice = CreateDictionary(kraje, stolice);
+            last(krajestolice);
+        }
+
+
+
+        public static List<int> CreateList(int dlugosc)
+        {
+            try
+            {
+                if (dlugosc <= 0)
+                {
+                    throw new ArgumentException("Dlugosc listy musi byc dodatnia");
+                }
+                return new List<int>(new int[dlugosc]);
+            }
+            catch (ArgumentException ex) 
+            {
+                Console.WriteLine("Błąd: " + ex.Message);
+                return new List<int>();
+            }
+        }
+
+
+        public static void FillList (List<int> list)
+        {
+            Random losowa = new Random();
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] =  losowa.Next(1, 101);
+            }
+        }
+
+
+        public static void PrintList (List<int> list)
+        {
+            Console.WriteLine("Lista wypelniona liczbami calkowitym od 1 do 100: " + string.Join(", ", list));
+        }
+
+
+        public static int[,] CreateMultiDimensionalarray(int dlugosc)
+        {
+            int[,] array = new int[dlugosc, dlugosc];
+            return array;
+        }
+
+
+        public static void fillarray(int[,] array)
+        {
+            Random losowa = new Random();
+            for(int i = 0;i < array.GetLength(0); i++)
+            {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = losowa.Next(1, 100);
+                }
+            }
+        }
+
+
+        public static void SumaPrzekatnej(int[,] array)
+        {
+            int suma = 0;
+            for (int i = 0; i < array.GetLength(0) ; i++)
+            {
+                suma = suma + array[i, i];
+            }
+            Console.WriteLine("Suma przkatnej wynosi: " + suma);
+        }
+
+        public static Dictionary<string, string> CreateDictionary(List<string> stolice, List<string> kraje)
+        {
+            Dictionary<string, string> krajestolice = new Dictionary<string, string>();
+            for(int i = 0; i < stolice.Count; i++)
+            {
+                krajestolice.Add(stolice[i], kraje[i]);
+            }
+            return krajestolice;
+        }
+        public static void last(Dictionary<string, string> slownik)    
+        {
+            bool valid = false;
+            do
+            {
+                try
+                {
+                    Console.Write("Podaj nazwe kraju lub \"q\" aby zakonczyc program : ");
+                    string c = Console.ReadLine();
+                    if (c == "q")
+                    {
+                        break;
+                    }
+                    {
+                        
+                    }
+                    Console.WriteLine("Stolica tego kraju to: " + slownik[c]);
+                    valid = true;
+                }
+                catch (KeyNotFoundException)
+                {
+                    Console.WriteLine("Bark tego panstwa w bazie danych");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("BŁĄD");
+                }
+            } while(!valid);
+            
+            
+
+        }
+    }
+}
