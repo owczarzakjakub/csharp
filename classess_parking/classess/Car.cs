aa31778268d78afa56db549b60cee3a92241551f
owczarzakjakub <144985790+owczarzakjakub@users.noreply.github.com>
2024-02-08 08:41:21 +0100

    Add files via upload

---
+++ classess_parking/classess/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace classess_parking.classess
+{
+    internal class Car
+    {
+        public string Brand { get; private set; }
+        public string Model { get; private set; }
+        public ushort Year { get; private set; }
+        public Color Color { get; private set; }
+        public void ShowInformation()
+        {
+            Console.WriteLine($"To jest {Brand} {Model} z {Year} o kolorze {Color}");
+        }
+    }
+}
---
+++ classess_parking/classess/Parking.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classess_parking.classess
+{
+    internal class Parking
+    {
+        public string name { get; set; }
+        public Car[] Cars { get; set; }
+
+        public void AddCar(Car car)
+        {
+            for (int i = 0; i < Cars.Length; i++)
+            {
+                if (Cars[i] == null)
+                {
+                    Cars[i] = car;
+                    Console.WriteLine("Dodano samochód na miejscu na miejscu parkingowym numer: {0}", i);
+                    return;
+                }
+                Console.WriteLine("Brak wolnych miejsc parkingowych.");
+            }
+        }
+        public void RemoveCar(int index)
+        {
+            if (index >= 0 && index < Cars.Length)
+            {
+                if (Cars[index] != null)
+                {
+                    Cars[index] = null;
+                    Console.WriteLine("Samochod został usuniety z danego miejsca parkingowego o numerze {0}", index);
+                }
+                else
+                {
+                    Console.WriteLine("W podanym miejscu parkingowym nie ma samochodu");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Błędnu numer miejsca postojowego an parkingu");
+            }
+        }
+        public void ShowCars()
+        {
+            Console.WriteLine($"Parking {name} ma {Cars.Length} miejsc parkingowych");
+            for (int i = 0; i < Cars.Length; i++)
+            {
+                if (Cars[i] != null)
+                {
+                    Console.WriteLine($"Miejsce parkingowe numer {i + 1}: ");
+                    Cars[i].ShowInformation();
+                }
+                else
+                {
+                    Console.WriteLine($"Wolne miejsce numer: {i + 1}");
+                }
+            }
+        }
+    }
+}
