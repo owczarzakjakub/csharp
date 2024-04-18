1710d5320b8d5ccf30f66acbfdc3ec5e0e1c3d83
owczarzakjakub <144985790+owczarzakjakub@users.noreply.github.com>
2024-04-18 08:42:38 +0200

    Add files via upload

--- 13_2_destruktory_symulator/Program.cs
+++ 13_2_destruktory_symulator/Program.cs
@@ -23,37 +23,19 @@ namespace _13_2_destruktor_symulator
         switch(choice)
         {
           case 1:
-            Console.Write("Podaj markę samochodu:");
-            string brand = Console.ReadLine();
-            Console.Write("Podaj model samochodu:");
-            string model = Console.ReadLine();
-            Car newCar = new Car(brand, model);
-            cars.Add(newCar);
-            carDictionary[cars.Count] = newCar;
-            Console.WriteLine("Dodano nowy samochód");
+            AddCar(cars, carDictionary);
             break;
           case 2:
-            Console.WriteLine("\nLista samochodów:");
-            foreach (Car car in cars)
-            {
-              Console.WriteLine($"{car.Brand} {car.Model}");
-            }
+            DisplayCars(carDictionary);
             break;
           case 3:
-            Console.Write("Podaj numer samochodu do jazdy:");
-            int carNumber = int.Parse(Console.ReadLine());
-            if (carDictionary.TryGetValue(carNumber, out Car selectedCar))
-            {
-              selectedCar.Drive();
-            }
-            else
-            {
-              Console.WriteLine("Nierawidłowy numer samochodu");
-            }
+                        DisplayCars(carDictionary);
+                        DriveCar(carDictionary);
+
+            
             break;
                     case 4:
-                        Console.WriteLine("\nPodaj numer samochodu do symulacji uszkodzen");
-                        int damagedCraNumber
+                        
                         break;
                     case 5:
                         break;
@@ -68,6 +50,21 @@ namespace _13_2_destruktor_symulator
       
       Console.ReadKey();
     }
+
+        private static void DriveCar(Dictionary<int, Car> carDictionary)
+        {
+            Console.Write("Podaj numer samochodu do jazdy:");
+            int carNumber = int.Parse(Console.ReadLine());
+            if (carDictionary.TryGetValue(carNumber, out Car selectedCar))
+            {
+                selectedCar.Drive();
+            }
+            else
+            {
+                Console.WriteLine("Nierawidłowy numer samochodu");
+            }
+        }
+
         static public void DisplayMenu()
         {
             Console.WriteLine("Menu symulatora jazdy samochodem");
@@ -78,20 +75,57 @@ namespace _13_2_destruktor_symulator
             Console.WriteLine("5. Zezłomuj samochód");
             Console.WriteLine("6. Wyjście");
         }
-        static public int GetUserInput()
+        static public int GetUserInput(Dictionary<int, Car> carDictionary = null)
         {
             while (true)
             {
                 Console.WriteLine("Wybierz opcje: ");
-                if (int.TryParse(Console.ReadLine(), out int choice))
+                if (int.TryParse(Console.ReadLine(), out int input))
                 {
-                    return choice;
+                    if (carDictionary == null || carDictionary.ContainsKey (input))
+                    {
+                        return input;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Numer samochodu nie istnieje w slowniku.");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Blednie wprowadzoen dane, sproboj ponownnie");
+                    Console.WriteLine("Nieprawidlowy format danych, sproboj ponownnie");
                 }
             }
         }
+        private static void AddCar(List<Car> cars, Dictionary<int, Car> carDictionary)
+        {
+            Console.Write("Podaj markę samochodu:");
+            string brand = Console.ReadLine();
+            Console.Write("Podaj model samochodu:");
+            string model = Console.ReadLine();
+            Car newCar = new Car(brand, model);
+            cars.Add(newCar);
+            carDictionary[cars.Count] = newCar;
+            Console.WriteLine("Dodano nowy samochód");
+        }
+        private static void DisplayCars(Dictionary<int, Car> carDicitionary)
+        {
+            if(carDicitionary.Count == 0)
+            {
+                Console.WriteLine("brak samochodow do wyswietlenia");
+            }
+            else
+            {
+                Console.WriteLine("\nLista samochodów:");
+                foreach (var carEntry in carDicitionary)
+                {
+                    int key = carEntry.Key;
+                    Car car = carEntry.Value;
+                    Console.WriteLine($"Klucz: {key}, marka: {car}");
+                }
+            }
+            
+        }
+
   }
 }
