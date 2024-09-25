7a39f38678762ccea43c6265d07198d14af7cbbb
owczarzakjakub <144985790+owczarzakjakub@users.noreply.github.com>
2024-09-25 15:43:18 +0200

    Add files via upload

---
+++ project_dziedziczenieKompozycjaBibliotek/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace project_dziedziczenieKompozycjaBibliotek
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+    public class Author : Person
+    {
+        public List<Book> BooksList { get; set; }
+        public Author(string firstname, string lastName) : base(firstname, lastName)
+        {
+            BooksList = new List<Book>();
+        }
+        public void AddBook(Book book)
+        {
+            BooksList.Add(book);
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public int PublicationYear { get; set; }
+        public Book(string title, Author author, int publicationYear)
+        {
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+        }
+    }
+    public class Reader : Person
+    {
+        public List<Book> BorrowedBookList { get; set; }
+
+        public Reader(string firstName, string lastName) : base(firstName, lastName)
+        {
+            BorrowedBookList = new List<Book>();
+        }
+
+        public void BorrowBook(Book book)
+        {
+            BorrowedBookList.Add(book);
+            Console.WriteLine($"Ksiazka {book} zostala wypozyczona");
+        }
+
+    } 
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Author author = new Author("Adam", "Mickiewicz");
+            Book book = new Book("Pan Tadeusz", author, 1834);
+            author.AddBook(book);
+
+            Console.ReadKey();
+        }
+    }
+}
