3bae4243141147f1bf4360a3daa97eaf45d425a1
owczarzakjakub <144985790+owczarzakjakub@users.noreply.github.com>
2024-12-04 15:43:11 +0100

    Add files via upload

---
+++ project4_delegaty_cz2/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace project4_delegaty_powiadomienia
+{
+    internal class Program
+    {
+        public delegate void NotificationHandler(string message);
+        public class EmailNotifier
+        {
+            public void SendEmail(string message)
+            {
+                Console.WriteLine($"Email wysłany: {message}");
+            }
+        }
+        public class SMSNotifier
+        {
+            public void SendSMS(string message)
+            {
+                Console.WriteLine($"SMS wysłany: {message}");
+            }
+        }
+
+        public class NotificationManager
+        {
+            public NotificationHandler Notify;
+            public void AddNotificationHandler( NotificationHandler handler )
+            {
+                Notify += handler;
+            }
+            public void RemoveNotificationHandler( NotificationHandler handler )
+            {
+                Notify -= handler;
+            }
+            public void SendNotification( string message )
+            {
+                Notify?.Invoke( message );
+            }
+        }
+
+        public class PushNotifier
+        {
+            public void SendPush( string message )
+            {
+                Console.WriteLine($"Powiadomienie push wysłane: {message}");
+            }
+        }
+        public static void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\nMenu");
+            Console.WriteLine("1. Dodaj powiadomienie email");
+            Console.WriteLine("2. Dodaj powiadomienie sms");
+            Console.WriteLine("3. Dodaj powiadomienie push");
+            Console.WriteLine("4. Usuń powiadomienie email");
+            Console.WriteLine("5. Usuń powiadomienie sms");
+            Console.WriteLine("6. Usuń powiadomienie push");
+            Console.WriteLine("7. Wyślij powiadomienia");
+            Console.WriteLine("8. Wyjście");
+            //int choice = ValidIntInput("Wybierz opcje");
+        }
+        static void Main(string[] args)
+        {
+            /*var emailNotifier = new EmailNotifier();
+            var smsNotifier = new SMSNotifier();
+            var pushNotifier = new PushNotifier();
+
+            var notificationManager = new NotificationManager();
+
+            notificationManager.AddNotificationHandler(emailNotifier.SendEmail);
+            notificationManager.AddNotificationHandler(smsNotifier.SendSMS);
+            
+
+            notificationManager.SendNotification("text1");
+
+
+            notificationManager.AddNotificationHandler(pushNotifier.SendPush);
+            notificationManager.RemoveNotificationHandler(smsNotifier.SendSMS);
+            notificationManager.SendNotification("text2");*/
+
+
+        }
+    }
+}
